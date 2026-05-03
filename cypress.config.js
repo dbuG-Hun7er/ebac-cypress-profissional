a4c3800a8f5af30a1641418455be9bd619938329
dbuG-Hun7er <dry_lucas@outlook.com>
2026-05-03 00:43:51 -0300

    feat: adiciona configuração do Cypress, Dockerfile e página de login  para o modulo 27

--- cypress.config.js
+++ cypress.config.js
@@ -7,6 +7,6 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: "http://lojaebac.ebaconline.art.br/",
+    baseUrl: "http://lojaebac.ebaconline.art.br/#",
   },
 });
